d4a5d346eac9ee04c2e2529be5ee26ad75f1b5ce
hopeyen <hopey1118@gmail.com>
2023-04-20 22:56:37 +0200

    chore: release v0.1.0

---
+++ scripts/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+VERSION="v$(cargo metadata --quiet --format-version 1 | jq -r '.packages[] | select(.name == "poi-radio") | .version')"
+
+if [[ -z "$VERSION" ]]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+auto-changelog --latest-version VERSION --release-summary
+
+(
+  git add CHANGELOG.md Cargo.toml Cargo.lock \
+    && git commit -m "chore: Bump version"
+) || true
+
+# Publish to crates.io
+cargo publish
